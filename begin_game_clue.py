e90af3edaeb6c000f51f2114b8c996385bfbf00a
niichapha <93215348+niichapha@users.noreply.github.com>
2021-11-01 12:53:42 -0500

    Update begin_game_clue.py

--- begin_game_clue.py
+++ begin_game_clue.py
@@ -9,6 +9,29 @@ def all_game_setup():
     action('CreatePlace(Castle, GreatHall)')
     action('CreatePlace(Alchemy, AlchemyShop)')
     action('CreatePlace(Weaponshop, Blacksmith)')
+
+# Create Characters
+
+    # Main Character(Witcher Evander)
+    action('CreateCharacter(Evander, B)')
+    action('SetEyeColor(Evander, Brown)', False)
+    action('SetHairStyle(Evander, Spiky)', False)
+    action('SetHairColor(Evander, Brown)', False)
+    action('SetClothing(Evander, Noble)', False)
+
+    # King Heinrich
+    action('CreateCharacter(King Heinrich, H)')
+    action('SetEyeColor(King Heinrich, Brown)', False)
+    action('SetHairStyle(King Heinrich, Mage_Full)', False)
+    action('SetHairColor(King Heinrich, Gray)', False)
+    action('SetClothing(King Heinrich, King)', False)
+
+    # Princess Aida
+    action('CreateCharacter(Princess Aida, C)')
+    action('SetEyeColor(Princess Aida, Blue)', False)
+    action('SetHairStyle(Princess Aida, Short)', False)
+    action('SetHairColor(Princess Aida, Brown)', False)
+    action('SetClothing(Princess Aida, Noble)', False)
     
 #creating clues for alchemist_shop
 
