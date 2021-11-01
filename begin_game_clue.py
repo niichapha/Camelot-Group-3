7e723ea3cf70bbf2daf633e3bcdc867c86f3d38f
vijaayyyyvarshini <93293346+vijaayyyyvarshini@users.noreply.github.com>
2021-11-01 12:45:47 -0500

    Update begin_game_clue.py

--- begin_game_clue.py
+++ begin_game_clue.py
@@ -1,4 +1,15 @@
+from action import action
 
+def all_game_setup():
+    action('SetTitle(\"The Destiny\")', False)
+    action("SetCredits(\"Experience Manager by Vijay Varshini Lakshmi Narayanan, Narendra Nath, Nichapha Manoonwong , Varun Venkatesh, Akshay Reddy\")", False)
+    
+# Create Game Locations
+    action('CreatePlace(BookPlace, Library)')
+    action('CreatePlace(Castle, GreatHall)')
+    action('CreatePlace(Alchemy, AlchemyShop)')
+    action('CreatePlace(Weaponshop, Blacksmith)')
+    
 #creating clues for alchemist_shop
 
 action('CreateItem(Energy Potion, PurplePotion)', False)
