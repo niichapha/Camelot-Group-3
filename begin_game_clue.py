786e53615e06757bd58c840689c16a752649b0d2
grimhandler09 <93528375+grimhandler09@users.noreply.github.com>
2021-11-01 08:54:43 -0500

    Add files via upload

---
+++ begin_game_clue.py
@@ -0,0 +1,7 @@
+
+#creating clues for alchemist_shop
+
+action('CreateItem(Energy Potion, PurplePotion)', False)
+
+#creating clues for blacksmith_shop
+action('CreateItem(Sword of Heisenberg , Sword)', False)
