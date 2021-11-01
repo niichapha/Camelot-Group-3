6002d9df51f3473c77911354d18deeaabfe90590
vijaayyyyvarshini <93293346+vijaayyyyvarshini@users.noreply.github.com>
2021-11-01 12:41:05 -0500

    Update alchemist_shop_setup.py

--- alchemist_shop_setup.py
+++ alchemist_shop_setup.py
@@ -3,13 +3,23 @@ from action import action
 def alchemist_shop_setup():
 	
 	action('StopSound()')
-	action('PlaySound(Serenity, Alch, True)')
-	
+	action('PlaySound(Serenity, AlchemyShop, True)')
 	action('Enter(Evander, AlchemyShop.Door, true)')
 	action('SetCameraFocus(Evander)')
 	action('SetCameraMode(follow)')
+	action('CreateItem(, BluePotion)')
+	action('CreateItem(Potion2, RedPotion)')
+	action('CreateItem(EnergyPotion, PurplePotion)')
+	action('SetPosition(Potion1, AlchemyShop.AlchemistTable.Center)', False)
+	action('SetPosition(Potion2, AlchemyShop.AlchemistTable.Right)', False)
+	action('SetPosition(Potion3, AlchemyShop.Table.FrontLeft)', False)
 	action('Pickup(Evander, BluePotion, AlchemyShop.AlchemistTable)')
 	action('Pickup(Evander, RedPotion, AlchemyShop.AlchemistTable)')
-	action('Pickup(Evander, PurplePotion, AlchemyShop.AlchemistTable)')
+	action('Pickup(Evander, PurplePotion, AlchemyShop.Table)')
 	action('Exit(Evander, AlchemyShop.Door, true)')
 	action('EnableInput()')
+	
+	#Enable effect in the environment 
+	action('EnableEffect(QueensCastle.Fireplace, Campfire)', False)
+	
+	
