227c9b47ae421498cf1ff13d5900fc84f6e72242
vijaayyyyvarshini <93293346+vijaayyyyvarshini@users.noreply.github.com>
2021-11-01 16:31:51 -0500

    Update Blacksmith_setup.py

--- Blacksmith_setup.py
+++ Blacksmith_setup.py
@@ -4,12 +4,12 @@ def Blacksmith_setup():
 	
 	# Start Background music for alchemy shop
 	action('StopSound()')
-	action('PlaySound(Serenity, WeaponShop, True)')
+	action('PlaySound(Serenity, WeaponShop, true)')
 	
 	action('Enter(Evander, WeaponShop.Door, true)')
 	action('SetCameraFocus(Evander)')
 	action('SetCameraMode(follow)')
-	action('OpenFurniture(Evander, Camp.Chest)
-	action('Pickup(Evander, Swords, WeaponShop.Chest)')
+	action('OpenFurniture(Evander, WeaponShop.Chest)')
+	action('Pickup(Evander, SwordofHeisenberg, WeaponShop.Chest)')
 	action('Exit(Evander, WeaponShop.Door, true)')
 	action('EnableInput()')
