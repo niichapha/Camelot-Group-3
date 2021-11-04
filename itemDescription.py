b245ac8503fcff276947a76655284565f30c7291
vijaayyyyvarshini <93293346+vijaayyyyvarshini@users.noreply.github.com>
2021-11-03 22:29:19 -0500

    Update itemDescription.py

--- itemDescription.py
+++ itemDescription.py
@@ -8,26 +8,26 @@ def inspect_item(item):
 	if item == 'BluePotion':
 		#BluePotion is HealingPotion
 		midscene_narration('The label reads - HealingPotion')
-		action('PlaySound(Unlock, BluePotion, false')
+		action('PlaySound(Unlock, BluePotion, false)')
 	
 	elif item == 'PurplePotion':
 		#PurplePotion is EnergyPotion
 		midscene_narration('The label reads - EnergyPotion')
-		action('PlaySound(Unlock, PurplePotion, false')
+		action('PlaySound(Unlock, PurplePotion, false)')
 		
 	
 	elif item == 'RedPotion':
 		#RedPotion is FirePotion
 		midscene_narration('The label reads - FirePotion')
-		action('PlaySound(Unlock, RedPotion, false')
+		action('PlaySound(Unlock, RedPotion, false)')
 
 
 	#Sword
 	elif item == 'Sword':
 		midscene_narration('The label reads - SwordofHeisenberg')
-		action('PlaySound(Draw, Sword, false')
+		action('PlaySound(Draw, Sword, false)')
 
 	#Hammer
 	elif item == 'Hammer':
 		midscene_narration('The label reads - HammerofHeisenberg')
-		action('PlaySound(Hammer, Hammer, false')
+		action('PlaySound(Hammer, Hammer, false)')
