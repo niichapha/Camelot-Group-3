65f89e2a86ba9512bb119e85f8982f87d76bcc63
unknown <nichapha.manoonwong@slu.edu>
2021-11-30 10:07:24 -0600

    CG3-39 update Alchemy name

--- alchemist_shop_setup.py
+++ alchemist_shop_setup.py
@@ -4,22 +4,22 @@ from dialogues import dialogue_char
 def alchemist_shop():
 	
 	action('StopSound()')
-	action('PlaySound(Serenity, alchemy, True)')
-	action('SetPosition(alchemist,alchemy.BackDoor)')
-	action('Enter(Evander, alchemy.Door, True)')
+	action('PlaySound(Serenity, Alchemy, True)')
+	action('SetPosition(alchemist,Alchemy.BackDoor)')
+	action('Enter(Evander, Alchemy.Door, True)')
 	action('SetCameraFocus(Evander)')
 	action('SetCameraMode(follow)')
 	action('EnableInput()')
 	dialogue_char('Evander: I want the Potions')
 	dialogue_char('Alchemist: Which ones?')
 	dialogue_char('I want a Blue, Red and Purple Potion')
-	action('SetPosition(EnergyPotion,alchemy.Bar.Left)')
-	action('SetPosition(HealingPotion,alchemy.Bar.Right)')
-	action('SetPosition(FirePotion,alchemy.Bar.Center)')
-	action('Pickup(Evander, EnergyPotion,alchemy.Bar.Left )')
-	action('Pickup(Evander, HealingPotion,alchemy.Bar.Right)')
-	action('Pickup(Evander, FirePotion,alchemy.Bar.Center)')
-	action('Exit(Evander, alchemy.Door, True)')
+	action('SetPosition(EnergyPotion,Alchemy.Bar.Left)')
+	action('SetPosition(HealingPotion,Alchemy.Bar.Right)')
+	action('SetPosition(FirePotion,Alchemy.Bar.Center)')
+	action('Pickup(Evander, EnergyPotion,Alchemy.Bar.Left )')
+	action('Pickup(Evander, HealingPotion,Alchemy.Bar.Right)')
+	action('Pickup(Evander, FirePotion,Alchemy.Bar.Center)')
+	action('Exit(Evander, Alchemy.Door, True)')
 
 
 
