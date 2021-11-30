a253e22fd39a2dab173e0e5f93da88e0a1f5504c
unknown <nichapha.manoonwong@slu.edu>
2021-11-30 09:58:53 -0600

    CG3-39 update location name

--- environment.py
+++ environment.py
@@ -2,7 +2,7 @@ from action1 import action
 from places import Places_game
 
 def create_places():
-    lib=Places_game('bookplace','Library')
-    cas=Places_game('castle','GreatHall')
-    alc=Places_game('alchemy','AlchemyShop')
-    weap=Places_game('weaponshop','BlacksmithShop')
+    lib=Places_game('BookPlace','Library')
+    cas=Places_game('Castle','GreatHall')
+    alc=Places_game('Alchemy','AlchemyShop')
+    weap=Places_game('WeaponShop','BlacksmithShop')
