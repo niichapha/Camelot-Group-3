fff2543319756bfcf5a63abc2dfea0e76c74dc1a
grimhandler09 <93528375+grimhandler09@users.noreply.github.com>
2021-11-30 20:57:52 -0600

    Delete Narration.py

--- Narration.py
+++
@@ -1,7 +0,0 @@
-from action import action
-
-def Message(Name, dialogue):
-    action('SetNarration('+Name+': '+dialogue+')')
-    action('ShowNarration')
-    action('Wait(3)')
-    action('HideNarration')
