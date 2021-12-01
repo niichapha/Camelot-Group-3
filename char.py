307cae6e941785026a62e18f0bde40ade074f737
grimhandler09 <93528375+grimhandler09@users.noreply.github.com>
2021-11-30 20:59:44 -0600

    Delete char.py

--- char.py
+++
@@ -1,8 +0,0 @@
-from action1 import action
-
-class Char:
-    def __init__(self,name,cloth,body,hair='Spiky'):
-        self.name = name
-        action('CreateCharacter('+name+','+body+')')
-        action('SetClothing('+name+', '+cloth+')')
-        action('SetHairStyle('+name+','+hair+')')
