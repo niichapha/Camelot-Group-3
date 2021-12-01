6a513fd1b31707d660fc4e5f62b4529948df1f35
grimhandler09 <93528375+grimhandler09@users.noreply.github.com>
2021-11-30 21:01:30 -0600

    Delete places.py

--- places.py
+++
@@ -1,7 +0,0 @@
-from action1 import action
-
-class Places_game:
-    def __init__(self,place_name,place):
-        self.name=place_name
-        self.place=place
-        action('CreatePlace('+place_name+','+place+')')
