9d3fdd88b052ebade150153065927aceec3f4d3c
grimhandler09 <93528375+grimhandler09@users.noreply.github.com>
2021-11-30 21:00:05 -0600

    Delete environment.py

--- environment.py
+++
@@ -1,8 +0,0 @@
-from action1 import action
-from places import Places_game
-
-def create_places():
-    lib=Places_game('BookPlace','Library')
-    cas=Places_game('Castle','GreatHall')
-    alc=Places_game('Alchemy','AlchemyShop')
-    weap=Places_game('WeaponShop','BlacksmithShop')
