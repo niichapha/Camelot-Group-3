d560f7b82d8d146a3884cf7a33032d49d4dee0bd
unknown <nichapha.manoonwong@slu.edu>
2021-12-01 23:22:15 -0600

    CG3-30 Adjust Sequence

--- attack.py
+++ attack.py
@@ -4,4 +4,7 @@ def attack(player_1 , player_2):
     action('WalkTo('+player_1+','+player_2+')')
     action('PlaySound(Draw)','False')
     action('Attack('+player_1+','+player_2+')')
+    action('PlaySound(Attack1)','False')
+    action('PlaySound(Death1)','False')
     action('Die('+player_2+')')
+    
