74e357b3d81b78f769950744938ad3b448970d4a
grimhandler9 <vvenkatesh@slu.edu>
2021-12-01 23:36:09 -0600

    CG3-33: Created A class for Location

--- create_location.py
+++ create_location.py
@@ -1,6 +1,6 @@
 from action import action
 
-class Create_location:
+class Create_location: #Created a class for location
     
    def __init__(self,place_name,place):
         self.name=place_name
