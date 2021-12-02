7779c1690c7f47864ea78dcb3bc0bfdf8fc0d9d6
Vijay Varshini Lakshmi Narayanan <vijayvarshinilakshminarayanan@Vijays-MacBook-Air.local>
2021-12-02 08:19:29 -0600

    CG3-36
    git pull

--- Narration.py
+++ Narration.py
@@ -1,5 +1,10 @@
 from action import action
 
+class Narration:
+    def __init__(self,Name,dialogue):
+        self.Name=Name
+        self.dialogue=dialogue
+
 def Message(Name, dialogue):
     action('SetNarration('+Name+': '+dialogue+')')
     action('ShowNarration')
