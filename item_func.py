7c30fb43168beb33f87a7ebe6cc382d050d5f258
Narendra Nath Jagarlamudi <j.narendranath1162000@gmail.com>
2021-12-07 20:54:16 -0600

    Deleting unnecessary files

--- item_func.py
+++
@@ -1,9 +0,0 @@
-from item_class import item
-from action import action
-
-def item_creation(item_name,item,position):
-    action('CreateItem('+item_name+','+item+')')
-    action('SetPosition('+item_name+','+position+')')
-
-item_obj=item()
-item_obj.item_creation()
