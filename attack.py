a7eb5eb1aa8055b0eea923991e0a71c522dfda6b
unknown <nichapha.manoonwong@slu.edu>
2021-12-09 09:00:54 -0600

    CG3-42 update

--- attack.py
+++ attack.py
@@ -2,9 +2,11 @@ from action import action
 
 def attack(player_1 , player_2):
     action('WalkTo('+player_1+','+player_2+')')
+    action('Draw('+player_1+',SwordofHeisenberg)')
     action('PlaySound(Draw)','False')
     action('Attack('+player_1+','+player_2+')')
     action('PlaySound(Attack1)','False')
     action('PlaySound(Death1)','False')
+    action('CreateEffect('+player_2+',Blood)')
     action('Die('+player_2+')')
     
